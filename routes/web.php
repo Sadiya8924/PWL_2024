ccea8c4b33bae5abb6d935510618f46752c34068
Sadiya8924 <sadiyamaritza8924@gmail.com>
2025-02-19 09:20:51 +0700

    praktikum percobaan 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello world';
+});
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/greeting', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'Nama saya Sadiya Maritza dan NIM saya 2341720147';
+});
+
+Route::get('/user/{Sadiya}', function ($Sadiya) {
+    return 'Nama saya '.$Sadiya;
+    });
+    
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/posts/articles/{id}', function
+($articlesId) {
+    return "Post Articles ke-: ".$articlesId;
+});
+
+Route::get('/user/{name?}', function ($name='Jhon') {
+    return 'Nama saya '.$name;
+});
